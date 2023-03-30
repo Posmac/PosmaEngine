e151760b5397cd5bb183d6968d0604d2e346a76d
Posmac Nicolai <n.posmac@dragonslake.com>
2023-03-30 18:30:42 +0300

    Init vk instance, selected gpu, created logical device and vk surface

--- Engine/Source/Vulkan/Vulkan.cpp
+++ Engine/Source/Vulkan/Vulkan.cpp
@@ -12,6 +12,7 @@ namespace psm
         InitVulkanInstace();
         CreateSurface(hInstance, hWnd);
         SelectPhysicalDevice();
+        CreateLogicalDevice();
     }
 
     void Vulkan::Deinit()
@@ -30,36 +31,18 @@ namespace psm
         applicationInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         applicationInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 
-        std::vector<const char*> layers =
-        {
-            "VK_LAYER_KHRONOS_validation",
-            "VK_LAYER_LUNARG_monitor",
-            "VK_LAYER_LUNARG_api_dump",
-            "VK_LAYER_LUNARG_core_validation",
-            "VK_LAYER_LUNARG_object_tracker",
-        };
-        VerifyLayersSupport(layers);
-
-        std::vector<const char*> extensions =
-        {
-            "VK_KHR_surface",
-            "VK_KHR_win32_surface",
-            "VK_KHR_swapchain"
-            "VK_KHR_get_physical_device_properties2",
-            "VK_EXT_debug_utils",
-            "VK_EXT_debug_report",
-        };
-        VerifyInstanceExtensionsSupport(extensions);
+        VerifyLayersSupport(m_ValidationLayers);
+        VerifyInstanceExtensionsSupport(m_InstanceExtensions);
 
         VkInstanceCreateInfo instanceCreateInfo{};
         instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         instanceCreateInfo.pNext = nullptr;
         instanceCreateInfo.flags = 0;
         instanceCreateInfo.pApplicationInfo = &applicationInfo;
-        instanceCreateInfo.enabledLayerCount = layers.size();
-        instanceCreateInfo.ppEnabledLayerNames = layers.data();
-        instanceCreateInfo.enabledExtensionCount = extensions.size();
-        instanceCreateInfo.ppEnabledExtensionNames= extensions.data();
+        instanceCreateInfo.enabledLayerCount = m_ValidationLayers.size();
+        instanceCreateInfo.ppEnabledLayerNames = m_ValidationLayers.data();
+        instanceCreateInfo.enabledExtensionCount = m_InstanceExtensions.size();
+        instanceCreateInfo.ppEnabledExtensionNames= m_InstanceExtensions.data();
 
         VkResult res = vkCreateInstance(&instanceCreateInfo, nullptr, &m_Instance);
         if (res != VK_SUCCESS)
@@ -89,10 +72,12 @@ namespace psm
                 if (strcmp(layerName, property.layerName) == 0)
                 {
                     actualLayersToEnable.push_back(layerName);
+                    std::cout << "Layer: " << layerName;
+                    std::cout << " is supported" << std::endl;
                 }
                 else
                 {
-                    std::cout << "Layer: " << layerName << " isn`t supported" << std::endl;
+                    //std::cout << " isn`t supported" << std::endl;
                 }
             }
         }
@@ -107,7 +92,7 @@ namespace psm
 
         if (availableExtensionsCount == 0)
         {
-            std::cout << "Failed to enumerate available extensions" << std::endl;
+            std::cout << "Failed to enumerate available instance extensions" << std::endl;
         }
 
         std::vector<VkExtensionProperties> availableExtensions(availableExtensionsCount);
@@ -122,10 +107,47 @@ namespace psm
                 if (strcmp(property.extensionName, extension) == 0)
                 {
                     actualExtensionsToEnable.push_back(extension);
+                    std::cout << "Instance extension: " << extension;
+                    std::cout << " is supported" << std::endl;
                 }
                 else
                 {
-                    std::cout << "Extension: " << extension << " isn`t supported" << std::endl;
+                    //std::cout << " isn`t supported" << std::endl;
+                }
+            }
+        }
+
+        extensionsToEnable = actualExtensionsToEnable;
+    }
+
+    void Vulkan::VerifyDeviceExtensionsSupport(std::vector<const char*>& extensionsToEnable)
+    {
+        uint32_t availableExtensionsCount;
+        vkEnumerateDeviceExtensionProperties(m_PhysicalDevice, nullptr, &availableExtensionsCount, nullptr);
+
+        if (availableExtensionsCount == 0)
+        {
+            std::cout << "Failed to enumerate available device extensions" << std::endl;
+        }
+
+        std::vector<VkExtensionProperties> availableExtensions(availableExtensionsCount);
+        vkEnumerateDeviceExtensionProperties(m_PhysicalDevice, nullptr, &availableExtensionsCount, availableExtensions.data());
+
+        std::vector<const char*> actualExtensionsToEnable;
+
+        for (auto& property : availableExtensions)
+        {
+            for (auto& extension : extensionsToEnable)
+            {
+                if (strcmp(property.extensionName, extension) == 0)
+                {
+                    actualExtensionsToEnable.push_back(extension);
+                    std::cout << "Device extension: " << extension;
+                    std::cout << " is supported" << std::endl;
+                }
+                else
+                {
+                    //std::cout << " isn`t supported" << std::endl;
                 }
             }
         }
@@ -166,13 +188,17 @@ namespace psm
         {
             VkPhysicalDeviceProperties deviceProps{};
             vkGetPhysicalDeviceProperties(device, &deviceProps);
-            
+
+            VkPhysicalDeviceFeatures deviceFeatures{};
+            vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
             if (deviceProps.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
             {
                 std::cout << "Found discrette device: " << std::endl;
                 std::cout << "Production: " << deviceProps.vendorID << ", " << deviceProps.deviceName << std::endl;
                 m_PhysicalDevice = device;
                 m_PhysicalDeviceProperties = deviceProps;
+                m_PhysicalDeviceFeatures = deviceFeatures;
             }
         }
 
@@ -180,10 +206,89 @@ namespace psm
         {
             VkPhysicalDeviceProperties deviceProps{};
             vkGetPhysicalDeviceProperties(physicalDevicesAvailable[0], &deviceProps);
+
+            VkPhysicalDeviceFeatures deviceFeatures{};
+            vkGetPhysicalDeviceFeatures(physicalDevicesAvailable[0], &deviceFeatures);
+
             std::cout << "Unable to get discrette gpu, selected first one" << std::endl;
             std::cout << "Production: " << deviceProps.vendorID << ", " << deviceProps.deviceName << std::endl;
             m_PhysicalDevice = physicalDevicesAvailable[0];
             m_PhysicalDeviceProperties = deviceProps;
+            m_PhysicalDeviceFeatures = deviceFeatures;
         }
+    }
+
+    void Vulkan::CreateLogicalDevice()
+    {
+        VerifyDeviceExtensionsSupport(m_DeviceExtensions);
+
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pNext = nullptr;
+        createInfo.flags = 0;
+        createInfo.enabledExtensionCount = m_DeviceExtensions.size();
+        createInfo.ppEnabledExtensionNames = m_DeviceExtensions.data();
+        createInfo.enabledLayerCount = m_ValidationLayers.size();
+        createInfo.ppEnabledLayerNames = m_ValidationLayers.data();
+        createInfo.pEnabledFeatures = &m_PhysicalDeviceFeatures;
+
+        uint32_t queueFamilyPropertyCount;
+        vkGetPhysicalDeviceQueueFamilyProperties(m_PhysicalDevice, &queueFamilyPropertyCount, nullptr);
+
+        if (queueFamilyPropertyCount == 0)
+        {
+            std::cout << "Failed to get physical device queue family properties" << std::endl;
+        }
+
+        std::vector<VkQueueFamilyProperties> availableQueueFamilyProperties(queueFamilyPropertyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(m_PhysicalDevice, &queueFamilyPropertyCount, availableQueueFamilyProperties.data());
+
+        int i = 0;
+        for (const auto& family : availableQueueFamilyProperties) 
+        {
+            VkBool32 presentSupport = false;
+            vkGetPhysicalDeviceSurfaceSupportKHR(m_PhysicalDevice, i, m_Surface, &presentSupport);
+            if (family.queueFlags & VK_QUEUE_GRAPHICS_BIT) 
+            {
+                m_QueueIndices.GraphicsFamily = i;
+            }
+            if (presentSupport) 
+            {
+                m_QueueIndices.PresentFamily = i;
+            }
+            if (m_QueueIndices.IsComplete())
+            {
+                break;
+            }
+            i++;
+        }
+
+        std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+        std::set<uint32_t> uniqueQueueFamilies = { m_QueueIndices.GraphicsFamily.value(),
+                                                  m_QueueIndices.PresentFamily.value() };
+        float queuePriority = 1.0f;
+
+        for (uint32_t family : uniqueQueueFamilies) 
+        {
+            VkDeviceQueueCreateInfo queueCreateInfo{};
+            queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueCreateInfo.queueFamilyIndex = family;
+            queueCreateInfo.queueCount = 1;
+            queueCreateInfo.pQueuePriorities = &queuePriority;
+            queueCreateInfos.push_back(queueCreateInfo);
+        }
+
+        createInfo.queueCreateInfoCount = queueCreateInfos.size();
+        createInfo.pQueueCreateInfos = queueCreateInfos.data();
+
+        VkResult result = vkCreateDevice(m_PhysicalDevice, &createInfo, nullptr, &m_LogicalDevice);
+
+        if (result != VK_SUCCESS)
+        {
+            std::cout << "Failed to create logical device" << std::endl;
+        }
+
+        vkGetDeviceQueue(m_LogicalDevice, m_QueueIndices.GraphicsFamily.value(), 0, &m_QueueIndices.GraphicsQueue);
+        vkGetDeviceQueue(m_LogicalDevice, m_QueueIndices.PresentFamily.value(), 0, &m_QueueIndices.PresentQueue);
     }
 }
--- Engine/Source/Vulkan/Vulkan.h
+++ Engine/Source/Vulkan/Vulkan.h
@@ -5,7 +5,9 @@
 #include <string>
 #include <iostream>
 #include <utility>
+#include <optional>
 #include <Windows.h>
+#include <set>
 
 #define VK_USE_PLATFORM_WIN32_KHR
 #include "Include/vulkan/vulkan.h"
@@ -17,6 +19,25 @@ namespace psm
     class Vulkan
     {
     public:
+        struct QueueFamilyIndices
+        {
+            std::optional<uint32_t> GraphicsFamily;
+            std::optional<uint32_t> PresentFamily;
+            VkQueue GraphicsQueue;
+            VkQueue PresentQueue;
+
+            bool IsComplete()
+            {
+                return GraphicsFamily.has_value() && PresentFamily.has_value();
+            }
+
+            bool IdenticalQueues()
+            {
+                return GraphicsFamily.value() == PresentFamily.value();
+            }
+        };
+
+    public:
         Vulkan();
         void Init(HINSTANCE hInstance, HWND hWnd);
         void Deinit();
@@ -24,13 +45,41 @@ namespace psm
     private:
         void VerifyLayersSupport(std::vector< const char*>& layersToEnable);
         void VerifyInstanceExtensionsSupport(std::vector<const char*>& extensionsToEnable);
+        void VerifyDeviceExtensionsSupport(std::vector<const char*>& extensionsToEnable);
         void CreateSurface(HINSTANCE hInstance, HWND hWnd);
         void SelectPhysicalDevice();
+        void CreateLogicalDevice();
     private:
+
+        std::vector<const char*> m_ValidationLayers =
+        {
+            "VK_LAYER_KHRONOS_validation",
+            "VK_LAYER_LUNARG_monitor",
+            "VK_LAYER_LUNARG_api_dump",
+            "VK_LAYER_LUNARG_core_validation",
+            "VK_LAYER_LUNARG_object_tracker",
+        };
+
+        std::vector<const char*> m_InstanceExtensions =
+        {
+            "VK_KHR_surface",
+            "VK_KHR_get_physical_device_properties2",
+            "VK_KHR_win32_surface",
+            "VK_EXT_debug_utils",
+            "VK_EXT_debug_report"
+        };
+
+        std::vector<const char*> m_DeviceExtensions =
+        {
+            "VK_KHR_swapchain",
+        };
+
         VkInstance m_Instance;
         VkPhysicalDevice m_PhysicalDevice;
         VkPhysicalDeviceProperties m_PhysicalDeviceProperties;
+        VkPhysicalDeviceFeatures m_PhysicalDeviceFeatures;
         VkDevice m_LogicalDevice;
         VkSurfaceKHR m_Surface;
+        QueueFamilyIndices m_QueueIndices;
     };
 }
